4f6c0c0d4ed3ce27c0961b1dc091357a03119cd4
Sean Hickey <hisean@media.mit.edu>
2020-04-28 11:58:36 -0400

    Added background and pen color selection and animation

--- editor.js
+++ editor.js
@@ -10,6 +10,13 @@ let path = [];
 let drawing = false;
 let drawingExists = false;
 
+let backgroundColor = '#000';
+let penColor = '#fff';
+
+ctx.lineWidth = 2.0;
+ctx.lineCap = 'round';
+ctx.lineJoin = 'round';
+
 discardButton.addEventListener('click', function(e) {
   if (drawingExists) {
     path = [];
@@ -21,7 +28,18 @@ discardButton.addEventListener('click', function(e) {
 
 sendButton.addEventListener('click', function(e) {
   if (drawingExists) {
-    drawingsRef.push(path);
+    // Normalize the color representations
+    ctx.fillStyle = backgroundColor;
+    let background = ctx.fillStyle;
+    ctx.fillStyle = penColor;
+    let pen = ctx.fillStyle;
+    
+    drawingsRef.push({
+      points: path,
+      backgroundColor: background,
+      penColor: pen
+    });
+    
     path = [];
     drawingExists = false;
     discardButton.disabled = true;
@@ -37,6 +55,30 @@ function distSq(x1, y1, x2, y2) {
 canvas.addEventListener('mousedown', function(e) {
   let x = e.offsetX;
   let y = e.offsetY;
+  
+  if (!drawing && !drawingExists) {
+    // Check for color selection
+    if (y <= 20) {
+      if (x < 180) {
+        let idx = Math.floor(x / 20);
+        if (idx === 0) {
+          backgroundColor = '#000';
+        }
+        else {
+          backgroundColor = `hsl(${Math.floor((idx - 1) * (360 / 8))}deg, 55%, 20%)`;
+        }
+      }
+      else if (x >= 420) {
+        let idx = Math.floor((x - 420) / 20);
+        if (idx === 8) {
+          penColor = '#fff';
+        }
+        else {
+          penColor = `hsl(${Math.floor(idx * (360 / 8))}deg, 55%, 65%)`;
+        }
+      }
+    }
+  }
   
   if (!drawing) {
     // Check to see if we should start drawing
@@ -67,14 +109,34 @@ canvas.addEventListener('mousemove', function(e) {
   }
 });
 
-ctx.fillStyle = `hsl(175deg, 55%, 55%)`;
-ctx.strokeStyle = `hsl(175deg, 55%, 55%)`;
-ctx.lineWidth = 2.0;
-ctx.lineCap = 'round';
-ctx.lineJoin = 'round';
-
 function draw() {
-  ctx.clearRect(0, 0, 600, 300);
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, 600, 300);
+  
+  if (!drawing && !drawingExists) {
+    // Background color selection
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, 20, 20);
+    for (let i = 0; i < 8; ++i) {
+      ctx.fillStyle = `hsl(${Math.floor(i * (360 / 8))}deg, 55%, 20%)`
+      ctx.fillRect((i * 20) + 20, 0, 20, 20);
+    }
+    
+    
+    // Pen color selection
+    for (let i = 0; i < 8; ++i) {
+      ctx.fillStyle = `hsl(${Math.floor(i * (360 / 8))}deg, 55%, 65%)`
+      ctx.fillRect(420 + (i * 20), 0, 20, 20);
+    }
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(580, 0, 20, 20);
+    
+    ctx.fillText('background color', 10, 35);
+    ctx.fillText('drawing color', 530, 35);
+  }
+  
+  ctx.fillStyle = penColor;
+  ctx.strokeStyle = penColor;
   
   if (path.length > 1) {
     ctx.beginPath();
--- viewer.js
+++ viewer.js
@@ -14,7 +14,6 @@ drawingsRef.on('child_added', function(data) {
   nextDrawingsQueue.push(data.val());
 });
 
-ctx.strokeStyle = `hsl(275deg, 55%, 55%)`;
 ctx.lineWidth = 2.0;
 ctx.lineCap = 'round';
 ctx.lineJoin = 'round';
@@ -23,10 +22,61 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+function clamp(val, min, max) {
+  return val < min ? min : (val > max ? max : val);
+}
+
+function lerp(start, finish, t) {
+  return ((finish - start) * t) + start;
+}
+
+function hexRGBtoHSL(hex) {
+  if (hex.startsWith('#')) {
+    hex = hex.slice(1);
+  }
+  hexInt = parseInt(hex, 16);
+  
+  let r = ((hexInt >> 16) & 0xFF) / 255.0;
+  let g = ((hexInt >>  8) & 0xFF) / 255.0;
+  let b = ((hexInt      ) & 0xFF) / 255.0;
+  
+  let colMax = Math.max(r, g, b);
+  let colMin = Math.min(r, g, b);
+  
+  let v = colMax;
+  let c = colMax - colMin;
+  let l = (colMax + colMin) / 2.0;
+  
+  let hue = 0;
+  if (c !== 0) {
+    if (v === r) {
+      hue = 60 * ((g - b) / c);
+    }
+    else if (v === g) {
+      hue = 60 * (2 + ((b - r) / c));
+    }
+    else {
+      hue = 60 * (4 + ((r - g) / c));
+    }
+  }
+  
+  let sat = 0;
+  if (l !== 0 && l !== 1) {
+    sat = c / (1 - Math.abs((2 * v) - c - 1));
+  }
+  
+  return {
+    h: clamp(Math.round(hue), 0, 360),
+    s: clamp(Math.round(sat * 100), 0, 100),
+    l: clamp(Math.round(l * 100), 0, 100)
+  };
+}
+
 function renderPercentageOfDrawingAtOffset(drawing, pct, xOffset) {
+  ctx.strokeStyle = drawing.penColor;
   ctx.beginPath();
-  let totalPtsToDraw = Math.floor(drawing.length * pct);
-  drawing.forEach((pt, idx) => {
+  let totalPtsToDraw = Math.floor(drawing.points.length * pct);
+  drawing.points.forEach((pt, idx) => {
     if (idx > totalPtsToDraw) {
       return;
     }
@@ -53,18 +103,23 @@ function getNextDrawing() {
 }
 
 const DRAWING_TIME_IN_MSEC = 5000;
+const BACKGROUND_TRANSITION_TIME_IN_MSEC = DRAWING_TIME_IN_MSEC / 2;
 
 let inFlight = [];
 
+let backgroundColor = '#000';
+let backgroundTransition = null;
+
 function draw(timestamp) {
-  ctx.clearRect(0, 0, 600, 300);
-  
   // Kick-off
   if (inFlight.length === 0) {
     let possibleDrawing = getNextDrawing();
     if (possibleDrawing !== undefined) {
+      backgroundColor = possibleDrawing.backgroundColor;
       inFlight.push({
-        points: possibleDrawing,
+        points: possibleDrawing.points,
+        backgroundColor: possibleDrawing.backgroundColor,
+        penColor: possibleDrawing.penColor,
         startTime: timestamp,
         lastDrawTime: timestamp,
       });
@@ -74,6 +129,10 @@ function draw(timestamp) {
   let toAdd = [];
   let toDeleteIndices = [];
   
+  // Draw background
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, 600, 300);
+  
   inFlight.forEach(function(drawing, idx) {
     let {points, startTime, lastDrawTime} = drawing;
     let prevPct = (lastDrawTime - startTime) / DRAWING_TIME_IN_MSEC;
@@ -81,8 +140,19 @@ function draw(timestamp) {
     
     // If we're finished drawing this one, start drawing another
     if (prevPct <= 1.0 && pct >= 1.0) {
+      let nextDrawing = getNextDrawing();
+      
+      backgroundTransition = {
+        from: hexRGBtoHSL(backgroundColor),
+        to: hexRGBtoHSL(nextDrawing.backgroundColor),
+        toHex: nextDrawing.backgroundColor,
+        startTime: timestamp
+      };
+      
       toAdd.push({
-        points: getNextDrawing(),
+        points: nextDrawing.points,
+        backgroundColor: nextDrawing.backgroundColor,
+        penColor: nextDrawing.penColor,
         startTime: timestamp,
         lastDrawTime: timestamp,
       });
@@ -93,10 +163,28 @@ function draw(timestamp) {
       toDeleteIndices.push(idx);
     }
     
+    // Draw lines
     let xOffset = (0.75 - pct) * CANVAS_WIDTH;
-    renderPercentageOfDrawingAtOffset(points, pct, xOffset);
+    renderPercentageOfDrawingAtOffset(drawing, pct, xOffset);
     drawing.lastDrawTime = timestamp;
-  })
+  });
+  
+  // Update background transition, if necessary
+  if (backgroundTransition !== null) {
+    let t = (timestamp - backgroundTransition.startTime) / BACKGROUND_TRANSITION_TIME_IN_MSEC;
+    if (t >= 1.0) {
+      // Transition over
+      backgroundColor = backgroundTransition.toHex;
+      backgroundTransition = null;
+    }
+    else {
+      let { from, to } = backgroundTransition;
+      let h = lerp(from.h, to.h, t);
+      let s = lerp(from.s, to.s, t);
+      let l = lerp(from.l, to.l, t);
+      backgroundColor = `hsl(${h}deg, ${s}%, ${l}%)`;
+    }
+  }
   
   // Remove old drawings
   if (toDeleteIndices.length > 0) {
